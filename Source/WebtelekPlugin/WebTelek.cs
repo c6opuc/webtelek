3e37f4c9f104ee4d629c5141ab55965ad047b848
saulyak <saulyak@4f13ad16-fd35-0410-a746-11af89934611>
2011-10-11 12:13:04 +0000

    ''

--- Source/WebtelekPlugin/WebTelek.cs
+++ Source/WebtelekPlugin/WebTelek.cs
@@ -51,7 +51,7 @@ namespace MediaPortal.GUI.WebTelek
         #endregion
         #region Variables
                 
-        public static string VERSION = "1.0.0.1";
+        public static string VERSION = "1.2.1.0";
         public static int PluginID  = 7926;
         public static int TVGuideID = 7927;
         public static int TVProgramID = 7928;
